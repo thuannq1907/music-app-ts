6d43488992833554c01b5739a0b7522f60cfe686
thuannq1907 <nguyenquangthuan190820@gmail.com>
2024-03-19 18:14:07 +0700

    Làm trang danh sách bài hát (bên client) - 1

---
+++ controllers/client/song.controller.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from "express";
+
+// [GET] /songs/:slugTopic
+export const list = async (req: Request, res: Response) => {
+  res.render("client/pages/songs/list", {
+    pageTitle: "Danh sách bài hát",
+  });
+};
