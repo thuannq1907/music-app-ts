960c18f9b14d1d573a0620f53fbc0fbeabdc6b5d
thuannq1907 <nguyenquangthuan190820@gmail.com>
2024-03-19 14:30:37 +0700

    Tạo routes

---
+++ routes/client/topic.route.ts
@@ -0,0 +1,14 @@
+import { Router, Request, Response } from "express";
+const router: Router = Router();
+
+import Topic from "../../models/topic.model";
+
+router.get("/", async (req: Request, res: Response) => {
+  const topics = await Topic.find({
+    deleted: false
+  });
+
+  res.render("client/pages/topics/index");
+});
+
+export const topicRoutes: Router = router;
