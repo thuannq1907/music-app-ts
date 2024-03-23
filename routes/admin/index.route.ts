8926c862150acafe424a6605803556d0109d353a
thuannq1907 <nguyenquangthuan190820@gmail.com>
2024-03-23 16:55:36 +0700

    Trang Quản lý chủ đề (bên admin)

--- routes/admin/index.route.ts
+++ routes/admin/index.route.ts
@@ -1,5 +1,6 @@
 import { Express } from "express";
 import { dashboardRoutes } from "./dashboard.route";
+import { topicRoutes } from "./topic.route";
 import { systemConfig } from "../../config/system";
 
 const adminRoutes = (app: Express): void => {
@@ -7,6 +8,8 @@ const adminRoutes = (app: Express): void => {
   const prefixAdmin = systemConfig.prefixAdmin;
 
   app.use(`/${prefixAdmin}/dashboard`, dashboardRoutes);
+
+  app.use(`/${prefixAdmin}/topics`, topicRoutes);
 
 };
 
