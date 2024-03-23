681845053f112274eb98e09fd7d2691d60b1e390
thuannq1907 <nguyenquangthuan190820@gmail.com>
2024-03-23 17:02:59 +0700

    Tạo trang Danh sách bài hát (bên admin)

--- routes/admin/index.route.ts
+++ routes/admin/index.route.ts
@@ -1,6 +1,7 @@
 import { Express } from "express";
 import { dashboardRoutes } from "./dashboard.route";
 import { topicRoutes } from "./topic.route";
+import { songRoutes } from "./song.route";
 import { systemConfig } from "../../config/system";
 
 const adminRoutes = (app: Express): void => {
@@ -10,6 +11,8 @@ const adminRoutes = (app: Express): void => {
   app.use(`/${prefixAdmin}/dashboard`, dashboardRoutes);
 
   app.use(`/${prefixAdmin}/topics`, topicRoutes);
+
+  app.use(`/${prefixAdmin}/songs`, songRoutes);
 
 };
 
