4d13a36137e8fc0f441185eeac45b8c5178822fc
thuannq1907 <nguyenquangthuan190820@gmail.com>
2024-03-24 16:27:11 +0700

    Trang Chỉnh sửa bài hát

--- routes/admin/song.route.ts
+++ routes/admin/song.route.ts
@@ -24,4 +24,18 @@ router.post(
   controller.createPost
 );
 
+router.get("/edit/:id", controller.edit);
+
+router.patch(
+  "/edit/:id",
+  upload.fields(
+    [
+      { name: 'avatar', maxCount: 1 },
+      { name: 'audio', maxCount: 1 }
+    ]
+  ),
+  uploadCloud.uploadFields,
+  controller.editPatch
+);
+
 export const songRoutes: Router = router;
